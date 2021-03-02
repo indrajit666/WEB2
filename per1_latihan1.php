dba91c771a9b2be4690da8a895295443a2178684
RZTHUB/WEB2 <79934802+indrajit666@users.noreply.github.com>
2021-03-03 01:53:29 +0700

    Update and rename README.md to per1_latihan1.php

---
+++ per1_latihan1.php
@@ -0,0 +1,11 @@
+# WEB2
+PERT1_LATIHAN1
+<?php
+$A = 123 ; // variable global
+function Test() {
+$A = "Test"; // variable local
+echo"Nilai A dalam fungsi = $A \n";
+}
+Test();
+echo"Nilai A luar fungsi = $A \n";
+?>
