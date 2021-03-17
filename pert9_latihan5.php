e4b3a58252dbe9375031706acc0dd03753d0eedc
RZTHUB/WEB2 <79934802+indrajit666@users.noreply.github.com>
2021-03-17 23:00:05 +0700

    Update pert9_latihan5.php

--- pert9_latihan5.php
+++ pert9_latihan5.php
@@ -1,28 +1,15 @@
 <HTML>
 <HEAD>
-<TITLE> Getdate </TITLE>
+<TITLE> Tanggal </TITLE>
 </HEAD>
 <BODY>
-<center>
-<h1>
+<font size="10px">
 <?php
-$sekarang = getdate();
-$bulan = $sekarang['month'];
-$hari = $sekarang ['mday'];
-$tahun = $sekarang ['year'];
-$jam = $sekarang['hours'];
-if ($jam <= 11) {
- echo "Selamat Pagi";
-} elseif ($jam > 11 and $jam <= 15) {
- echo "Selamat Siang";
-} elseif ($jam > 15 and $jam <= 18) {
- echo "Selamat Sore";
-} elseif ($jam > 18) {
- echo "Selamat Malam";
-}
+echo "Sekarang tanggal ";
+echo date('d-F-Y');
+echo "<br>dan jam ";
+echo date('h:i:s A');
 ?>
-</h1>
-<h2> Selamat datang</h2>
-<h3> Sekarang adalah tanggal <?php echo "$hari $bulan $tahun"; ?></h3>
+</FONT>
 </BODY>
 </HTML>
